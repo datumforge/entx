e43bf9400055c21c304990b7a1cc5b152792281f
Matt Anderson <42154938+matoszz@users.noreply.github.com>
2024-02-28 12:02:48 -0700

    add vanilla ent example and basic instructions (#12)
    * add vanilla ent example and basic instructions
    
    * add vanilla sonar exclusion

---
+++ vanilla/_example/ent/entc.go
@@ -0,0 +1,42 @@
+//go:build ignore
+
+package main
+
+import (
+	"log"
+
+	"entgo.io/contrib/entgql"
+	"entgo.io/ent/entc/gen"
+	"go.uber.org/zap"
+
+	"entgo.io/ent/entc"
+)
+
+func main() {
+	gqlExt, err := entgql.NewExtension(
+		// Tell Ent to generate a GraphQL schema for
+		// the Ent schema in a file named ent.graphql.
+		entgql.WithSchemaGenerator(),
+		entgql.WithSchemaPath("../schema/ent.graphql"),
+		entgql.WithConfigPath("../gqlgen.yml"),
+		entgql.WithWhereInputs(true),
+	)
+	if err != nil {
+		log.Fatalf("creating entgql extension: %v", err)
+	}
+
+	if err := entc.Generate("./schema",
+		&gen.Config{
+			Features: []gen.Feature{gen.FeaturePrivacy},
+		},
+		entc.Dependency(
+			entc.DependencyName("Logger"),
+			entc.DependencyType(zap.SugaredLogger{}),
+		),
+		entc.Extensions(
+			gqlExt,
+		),
+	); err != nil {
+		log.Fatal("running ent codegen:", err)
+	}
+}
