31602001337933289af8dc48c3751a108fd07f8d
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2024-05-03 15:33:55 -0600

    feat: add mapping id to id mixin (#57)
    * feat: add mapping id to id mixin
    
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
    
    * mapping_id should also be unique
    
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- mixin/idmixin.go
+++ mixin/idmixin.go
@@ -1,6 +1,7 @@
 package mixin
 
 import (
+	"entgo.io/contrib/entgql"
 	"entgo.io/contrib/entoas"
 	"entgo.io/ent"
 	"entgo.io/ent/schema/field"
@@ -12,14 +13,32 @@ import (
 // IDMixin holds the schema definition for the ID
 type IDMixin struct {
 	mixin.Schema
+	// ExcludeMappingID to exclude the mapping ID field to the schema that can be used without exposing the primary ID
+	// by default, it is included in any schema that uses this mixin.
+	ExcludeMappingID bool
 }
 
 // Fields of the IDMixin.
-func (IDMixin) Fields() []ent.Field {
-	return []ent.Field{
+func (i IDMixin) Fields() []ent.Field {
+	fields := []ent.Field{
 		field.String("id").
 			Immutable().
 			Annotations(entoas.Annotation{ReadOnly: true}).
 			DefaultFunc(func() string { return ulids.New().String() }),
 	}
+
+	if !i.ExcludeMappingID {
+		fields = append(fields,
+			field.String("mapping_id").
+				Immutable().
+				Annotations(
+					entoas.Skip(true),
+					entgql.Skip(),
+				).
+				Unique().
+				DefaultFunc(func() string { return ulids.New().String() }),
+		)
+	}
+
+	return fields
 }
